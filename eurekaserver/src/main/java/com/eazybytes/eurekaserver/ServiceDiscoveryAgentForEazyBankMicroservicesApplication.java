42a6ed165129af57f75347e6c382df8266448673
Balaji.Ballani <ballanibalaji@gmail.com>
2024-04-19 18:48:56 +0530

    Eureka server

---
+++ eurekaserver/src/main/java/com/eazybytes/eurekaserver/ServiceDiscoveryAgentForEazyBankMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryAgentForEazyBankMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryAgentForEazyBankMicroservicesApplication.class, args);
+	}
+
+}
---
+++ eurekaserver/src/test/java/com/eazybytes/eurekaserver/ServiceDiscoveryAgentForEazyBankMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryAgentForEazyBankMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
