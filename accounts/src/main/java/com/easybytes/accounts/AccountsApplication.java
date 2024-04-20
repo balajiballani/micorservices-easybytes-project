6b32fb8a2c44d29f79e5438d3dd890587022d0a7
Balaji.Ballani <ballanibalaji@gmail.com>
2024-04-20 17:49:50 +0530

    Eureka server

--- accounts/src/main/java/com/easybytes/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/easybytes/accounts/AccountsApplication.java
@@ -1,18 +1,26 @@
 package com.easybytes.accounts;
 
 import com.easybytes.accounts.dto.AccountContactInfo;
+import com.netflix.discovery.EurekaClient;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareRef")
 @EnableConfigurationProperties(value = {AccountContactInfo.class})
+@EnableFeignClients
 public class AccountsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountsApplication.class, args);
     }
+
+
 
 }
